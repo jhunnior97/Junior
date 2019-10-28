d3f03927ddf85589856c95ff194cc50582881682
jhunnior97 <everaldo.m.l.j@gmail.com>
2019-10-28 08:53:55 -0400

    Teste 1

---
+++ client/main.js
@@ -0,0 +1,81 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+//
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+        Router.go('home');
+    }
+});
+//
+
+//
+Template.navigation.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('login');
+    }
+})
+//
+
+//
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password);
+    }
+});
+//
+
+//
+Meteor.loginWithPassword(email, password, function(error){
+    if(error){
+        console.log(error.reason);
+    } else {
+        Router.go("home");
+    }
+});
+//
+
+//
+Accounts.createUser({
+    email: email,
+    password: password
+}, function(error){
+    if(error){
+        console.log(error.reason); // Output error if registration fails
+    } else {
+        Router.go("home"); // Redirect user if registration succeeds
+    }
+});
+//
